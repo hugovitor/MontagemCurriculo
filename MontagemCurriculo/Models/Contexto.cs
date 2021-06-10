d4819ee4fba3e20efd20a157b8c786a82fb143f0
hugo.nunes <hugo.nunes@ssp.df.gov.br>
2021-06-10 15:57:34 -0300

    configurações

---
+++ MontagemCurriculo/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MontagemCurriculo.Models
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> opcoes): base(opcoes)
+        {
+
+        }
+    }
+}
--- MontagemCurriculo/Startup.cs
+++ MontagemCurriculo/Startup.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MontagemCurriculo.Models;
+using Rotativa.AspNetCore;
 
 namespace MontagemCurriculo
 {
@@ -24,6 +28,20 @@ namespace MontagemCurriculo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("Conexao")));
+            services.AddSession(opcoes =>
+            {
+                opcoes.IdleTimeout = TimeSpan.FromDays(1);
+
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(opcoes =>
+            {
+                opcoes.LoginPath = "/Usuarios/Login";
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -48,15 +66,19 @@ namespace MontagemCurriculo
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            RotativaConfiguration.Setup(env);
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Usuarios}/{action=Registro}/{id?}");
             });
         }
     }
