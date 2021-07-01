7a64d828cffbc999b75751fccdcdd53fec625b08
hugo.nunes <hugo.nunes@ssp.df.gov.br>
2021-07-01 15:43:22 -0300

    Onjetivos ViewComponent

---
+++ MontagemCurriculo/ViewComponents/ObjetivosViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MontagemCurriculo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MontagemCurriculo.ViewComponents
+{
+    public class ObjetivosViewComponent : ViewComponent
+    {
+        private readonly Contexto _contexto;
+
+        public ObjetivosViewComponent(Contexto contexo)
+        {
+            _contexto = contexo;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            return View(await _contexto.Objetivos.Where(o => o.CurriculoId == id).ToListAsync());
+        }
+    }
+}
