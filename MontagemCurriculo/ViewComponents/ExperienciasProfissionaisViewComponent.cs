d0a42747773637f918d2fd1d9cbb97154b3cead2
hugo.nunes <hugo.nunes@ssp.df.gov.br>
2021-07-02 11:47:44 -0300

    Experiências Profissionais ViewComponent

---
+++ MontagemCurriculo/ViewComponents/ExperienciasProfissionaisViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MontagemCurriculo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MontagemCurriculo.ViewComponents
+{
+    public class ExperienciasProfissionaisViewComponent : ViewComponent
+    {
+
+        private readonly Contexto _contexto;
+
+        public ExperienciasProfissionaisViewComponent(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            return View(await _contexto.ExperienciasProfissionais.Where(ep => ep.CurriculoId == id).ToListAsync());
+        }
+    }
+}
